58a21eed2667c2de229307a4d4f2487619dac752
Abdurrahman Ekecik <abdurrahmanekecik@gmail.com>
2023-02-09 11:34:01 +0300

    ItemController Added

---
+++ app/Http/Controllers/Api/ItemController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Item;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       $items = Item::all();
+       return $items;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $item = new İtem();
+        $item->name = $request->name;
+        $item->description = $request->description;
+        $item->time = $request->time;
+        $item->save();
+        return response()->json(['message'=>'Item Added']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $item = Item::($id);
+        return $item;
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $item = Item::findOrFail($request->id);
+        $item->name = $request->name;
+        $item->description = $request->description;
+        $item->time = $request->time;
+        $item->save();
+        return $item;
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $item = Item::destroy($id);
+        return response()->json(['message'=>'Delete Completed.']);
+    }
+}
